0cd91967c032ec0dbb3f3876d31c53800dcb29cb
Vanessa Vazquez <85457033+Vvazq1@users.noreply.github.com>
2021-06-15 15:33:04 +0000

    UPDATED errors

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,46 +5,43 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = input.question('What is your name?');
+const candidateName = input.question(' What is your name? ');
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
+let question; 'Who was the first American woman in space? '
 let correctAnswer; 'Sally Ride'
-let candidateAnswer;
-let questions; 'Who was the first American woman in space? '
+let candidateAnswer; ''
+let questions; 
 let correctAnswers;
 let candidateAnswers; 
 
 
 function askForName(candidateName) {
   // TODO 1.1b: Ask for candidate's name //
- input.question = candidateName; 
- }
+} 
   // TODO 1.1c: Say Hello to candidate  //
-  if (candidateName) {
-
-   console.log('Greetings,', candidateName, '!');
+   {
+   console.log('Greetings,',candidateName,'!');
 }
-
 
 
 function askQuestion(question) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log('Who was the first American woman in space?');
- // i am a little confuedon this part//
- candidateAnswer
-
+} const answer = input.question('Who is the first American woman in space? ')
+{
+console.log('Your answer is', answer);
 }
 
 
-function gradeQuiz(candidateAnswers) {
-
+function gradeQuiz(candidateAnswer) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  let correctAnswer = true
-if (correctAnswer) {
+ 
+if (answer === ('Sally Ride')) {
 console.log('Correct!');
-} else {
-  console.log('Wrong.');
+
+} else  {
+
+  console.log('Incorrect');
 }
 
   let grade;
